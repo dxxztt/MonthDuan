4aa0d144cc9c77c61b425de8a9b7247edc22c1c0
LENOVO <2371764759@qq.com>
2018-08-25 08:14:33 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lenovo/monthduan/LiushiView.java
@@ -0,0 +1,24 @@
+package com.example.lenovo.monthduan;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
+public class LiushiView extends ViewGroup {
+    public LiushiView(Context context) {
+        super(context);
+    }
+
+    public LiushiView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LiushiView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
